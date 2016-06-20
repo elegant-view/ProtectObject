d0287333426516bfdf890d98d9b9b33439a3e108
yibuyisheng <yibuyisheng@163.com>
2016-06-20 21:26:09 +0800

    init

---
+++ src/OrderedProtectObject.js
@@ -0,0 +1,41 @@
+/**
+ * @file 添加一种排序功能
+ * @author yibuyisheng(yibuyisheng@163.com)
+ */
+
+import ProtectObject from './ProtectObject';
+
+const ORDER = Symbol('order');
+const ORDER_CACHE = Symbol('orderCache');
+
+export default class OrderedProtectedObject extends ProtectObject {
+    constructor(...args) {
+        super(...args);
+
+        this[ORDER] = [];
+        this[ORDER_CACHE] = [];
+    }
+
+    set(key, value) {
+        super.set(key, value);
+        this.isLocked() ? this[ORDER_CACHE].push(key) : this[ORDER].push(key);
+    }
+
+    safeIterate(fn, context) {
+        if (!fn) {
+            return;
+        }
+
+        this.lock();
+        for (let i = 0, il = this[ORDER].length; i < il; ++i) {
+            const key = this[ORDER][i];
+            const value = this.get(key);
+
+            const result = fn.call(context, value, key);
+            if (result) {
+                break;
+            }
+        }
+        this.unlock();
+    }
+}
